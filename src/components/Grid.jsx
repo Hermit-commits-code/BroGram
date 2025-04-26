f7a9a306183e38e6020d85d26b5ba0027df8504d
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-04-26 12:42:31 -0400

    Finished Workout Card Component. Moving to Modal Component

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -15,8 +15,27 @@ export default function Grid() {
             : 'Legs';
 
         const trainingPlan = training_plan[workout];
+        const dayNum =
+          workoutIndex / 8 <= 1 ? '0' + (workoutIndex + 1) : workoutIndex + 1;
+        const icon =
+          workoutIndex % 3 === 0 ? (
+            <i className="fa-solid fa-dumbbell"></i>
+          ) : workoutIndex % 3 === 1 ? (
+            <i className="fa-solid fa-weight-hanging"></i>
+          ) : (
+            <i className="fa-solid fa-bolt"></i>
+          );
         if (workoutIndex === selectedWorkout) {
-          return <WorkoutCard key={workoutIndex} trainingPlan={trainingPlan} type={type} workoutIndex={workoutIndex} />;
+          return (
+            <WorkoutCard
+              key={workoutIndex}
+              trainingPlan={trainingPlan}
+              type={type}
+              workoutIndex={workoutIndex}
+              icon={icon}
+              dayNum={dayNum}
+            />
+          );
         }
         return (
           <button
@@ -24,22 +43,9 @@ export default function Grid() {
             key={workoutIndex}
           >
             <div className="plan-card-header">
-              <p>
-                Day{' '}
-                {workoutIndex / 8 <= 1
-                  ? '0' + (workoutIndex + 1)
-                  : workoutIndex + 1}
-              </p>
+              <p>Day{dayNum}</p>
             </div>
-            {isLocked ? (
-              <i className="fa-solid fa-lock"></i>
-            ) : workoutIndex % 3 === 0 ? (
-              <i className="fa-solid fa-dumbbell"></i>
-            ) : workoutIndex % 3 === 1 ? (
-              <i className="fa-solid fa-weight-hanging"></i>
-            ) : (
-              <i className="fa-solid fa-bolt"></i>
-            )}
+            {isLocked ? <i className="fa-solid fa-lock"></i> : icon}
             <div className="plan-card-header">
               <h4>
                 <b>{type}</b>
