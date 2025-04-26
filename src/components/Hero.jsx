fe0aeb82410d5775e1fa247a101a120fd3bde270
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-04-26 03:18:35 -0400

    chore: got the hero finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,56 @@ export default function Hero() {
         <li>Learn more about gym, training, and technique</li>
         <li>Become a lifetime gym bro</li>
       </ol>
+      <h3>THE RULES</h3>
+      <p>
+        To complete this program <b>YOU</b> must follow these 3 simple rules:
+      </p>
+      <ul className="rules-list">
+        <div className="rule-item">
+          <p>
+            <b>Rest</b>
+          </p>
+          <p>Ensure you are taking rest days where necessary.</p>
+        </div>
+        <div className="rule-item">
+          <p>
+            <b>Repetition</b>
+          </p>
+          <p>
+            Every rep is a pause rep following a{' '}
+            <abbr title="2 seconds down - 2 seconds pause - 2 seconds up">
+              2 - 2 - 2 tempo
+            </abbr>
+          </p>
+        </div>
+        <div className="rule-item">
+          <p>
+            <b>Weight*</b>
+          </p>
+          <p>
+            Select the maximum weight that allows you to complete the set with
+            good form.
+          </p>
+        </div>
+      </ul>
+      <small>
+        * The first and second set should be at 75% and 85% of your working
+        weight used in the last two sets.
+      </small>
+      <h3>The Training Plan</h3>
+      <p>
+        This training plan uses a structure known as the <b>Bro Split</b>, and
+        follows this rotation ⬇️
+      </p>
+      <p>
+        <b>
+          <i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i>
+        </b>
+      </p>
+      <p>
+        Complete all of the workouts below and track your progress along the way
+        ✅
+      </p>
     </>
   );
 }
